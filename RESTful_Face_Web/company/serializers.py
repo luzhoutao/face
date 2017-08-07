06ab686e3c07f9cb4967f11a068c4827f23673d4
xhoutao <luzhoutao@gmail.com>
2017-08-07 21:21:44 +0800

    generate CompanyId

--- RESTful_Face_Web/company/serializers.py
+++ RESTful_Face_Web/company/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 # model
 from django.contrib.auth.models import User
 # utils
-from .utils import generate_unique_id
+from .utils import generate_unique_id, admin
 
 class CompanySerializer(serializers.HyperlinkedModelSerializer):
     CompanyId = serializers.CharField(source="first_name", read_only=True)
@@ -16,9 +16,8 @@ class CompanySerializer(serializers.HyperlinkedModelSerializer):
         user = User(
             email=validated_data['email'],
             username=validated_data['username'],
+            first_name=generate_unique_id(admin),
         )
         user.set_password(validated_data['password'])
-        user.save()
-        user.first_name = generate_unique_id(user)
         user.save()
         return user
